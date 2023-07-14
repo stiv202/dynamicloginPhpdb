429561de7901e2fc686ac33e3d9897acf4e566f9
stiv202 <stiv202@gmail.com>
2023-07-14 15:02:57 +0400

    Update login.php

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ include('login_back.php') ?>
 
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="home.html">HOME</a>
+    <a class="navbar-brand" href="index.html">HOME</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
